14290bb3e86096b0d1f099cf6bf50ffb8e148f00
nazirmithu <nazirmithu22@gmail.com>
2023-05-04 20:54:01 +0600

    readme file add and rating add

---
+++ README.md
@@ -0,0 +1,15 @@
+# Dream Job Hub
+
+The Project Live Link is Here: [Click Here To Visit Website](https://roaring-raindrop-f81b8a.netlify.app/).
+
+## Project Task
+
+-  Home Page Section 1. Set NavBar.
+-  Home Page Section 2.  Banner Set with detail.
+-  Home Page Section 3. Display All Chef Details.-  
+-  Set Blog Page with 4 Questions Answer.
+-  Set Chef Banner Section
+-  Use React Tailwind and Vanilla css.
+-  Create JSON Data.
+-  Use Nested Routing to Display Individual Items.
+-  Set Toast For Favorite Recipe.
--- src/pages/DetailsData/ChefRecipe.jsx
+++ src/pages/DetailsData/ChefRecipe.jsx
@@ -3,6 +3,8 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
+import { Rating } from '@smastrom/react-rating';
+import '@smastrom/react-rating/style.css';
 // eslint-disable-next-line react/prop-types
 const ChefRecipe = ({ recipe }) => {
     // eslint-disable-next-line react/prop-types
@@ -15,39 +17,41 @@ const ChefRecipe = ({ recipe }) => {
     }
 
     return (
-        <div className='card card-compact  bg-purple-50 shadow-xl'>
-            <h1 className='text-center font-bold p-4'>{name}</h1>
-            <img src={recipe_url} alt="" />
-            <div className="overflow-x-auto">
-                <table className="table w-full">
-                    <thead>
-                        <tr className='text-center'>
-                            <th>Name</th>
-                            <th>Quantity</th>
-                        </tr>
-                    </thead>
-                    <tbody className='text-center'>
-                        {
-                            ingredients.slice(5).map((items, index) => <tr>
-                                <td>{items.name}</td>
-                                <td>{items.quantity}</td>
+        <div>
+            <div className='card card-compact  bg-purple-50 shadow-xl'>
+                <h1 className='text-center font-bold p-4'>{name}</h1>
+                <img src={recipe_url} alt="" />
+                <div className="overflow-x-auto">
+                    <table className="table w-full">
+                        <thead>
+                            <tr className='text-center'>
+                                <th>Name</th>
+                                <th>Quantity</th>
                             </tr>
-                            )
-                        }
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody className='text-center'>
+                            {
+                                ingredients.slice(5).map((items, index) => <tr>
+                                    <td>{items.name}</td>
+                                    <td>{items.quantity}</td>
+                                </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+                <div>
+                    <p className='text-center font-bold p-4'>Cooking Method</p>
+                    {
+                        cooking_method.map(method => <p className='p-4 text-center'>
+                            {method}
+                        </p>)
+                    }
+                    <Rating style={{ maxWidth: 250 }} value={rating} readOnly />
+                </div>
+                <button onClick={favoriteButton} disabled={disabled} className='btn bg-purple-900'>Favorite button</button>
+                <Toaster />
             </div>
-            <div>
-                <p className='text-center font-bold p-4'>Cooking Method</p>
-                {
-                    cooking_method.map(method => <p className='p-4 text-center'>
-                        {method}
-                    </p>)
-                }
-                <p>Rating: {rating}</p>
-            </div>
-            <button onClick={favoriteButton} disabled={disabled} className='btn bg-purple-900'>Favorite button</button>
-            <Toaster />
         </div>
     );
 };
