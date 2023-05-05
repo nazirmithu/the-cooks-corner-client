4bba74e45fa15750e42a1693ab973e9b123adca8
nazirmithu <nazirmithu22@gmail.com>
2023-05-05 19:50:20 +0600

    home page second extra section add

--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -5,6 +5,10 @@ import Marquee from "react-fast-marquee";
 import image1 from '../../assets/pic-1.jpg';
 import image2 from '../../assets/pic-2.jpg';
 import image3 from '../../assets/pic-3.png';
+import image4 from '../../assets/Desserts.jpg';
+import image5 from '../../assets/drinks.jpg';
+import image6 from '../../assets/Bread.jpg';
+import image7 from '../../assets/salad.jpg';
 
 const Order = () => {
     return (
@@ -24,7 +28,44 @@ const Order = () => {
                     </div>
                     <div className="carousel-item">
                         <img className='w-96 h-96' src={image3} alt="Burger" />
-                    </div>                    
+                    </div>
+                </div>
+            </div>
+            <div className='bg-purple-100'>
+                <h1 className='text-3xl font-bold my-12 text-center mt-20'>Browse By Categories</h1>
+                <div className='flex gap-4 max-w-screen-lg mx-auto'>
+                    <div className='text-center'>
+                        <div className="avatar ">
+                            <div className="w-64 mask mask-squircle">
+                                <img src={image4} />
+                            </div>
+                        </div>
+                        <p className='text-xl font-bold'>Desserts</p>
+                    </div>
+                    <div className='text-center'>
+                        <div className="avatar ">
+                            <div className="w-64 mask mask-squircle">
+                                <img src={image5} />
+                            </div>
+                        </div>
+                        <p className='text-xl font-bold'>Drinks</p>
+                    </div>
+                    <div className='text-center'>
+                        <div className="avatar ">
+                            <div className="w-64 mask mask-squircle">
+                                <img src={image6} />
+                            </div>
+                        </div>
+                        <p className='text-xl font-bold'>Bread</p>
+                    </div>
+                    <div className='text-center'>
+                        <div className="avatar ">
+                            <div className="w-64 mask mask-squircle">
+                                <img src={image7} />
+                            </div>
+                        </div>
+                        <p className='text-xl font-bold'>salad</p>
+                    </div>
                 </div>
             </div>
         </div>
